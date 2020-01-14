359a19d218aee6ed9d20f5a18d46cbda4b5feef6
Sanju Kumar <S4njuKumar@gmail.com>
2020-01-15 01:04:52 +0530

    imported files and connected with the database

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+// import the files
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');  
+
+require('dotenv').config();
+
+// use the imported files
+const app = express();
+const port = process.env.PORT||5000;
+
+app.use(cors());
+app.use(express.json()); //body parser 
+
+// mongodb connection
+const uri = process.env.LOCAL_URI
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open',()=>{
+	console.log('connectedto database mongodb @ 27017');
+});
+
+//to check whether server is running
+app.listen(port, ()=>{
+	console.log('Server is running on port:',port);
+});

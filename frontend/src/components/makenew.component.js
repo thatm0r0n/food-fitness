3de266b9b634c053b59853da402eeb8eaffb0f3d
Sanju Kumar <S4njuKumar@gmail.com>
2020-01-16 22:16:20 +0530

    added the forms

--- frontend/src/components/makenew.component.js
+++ frontend/src/components/makenew.component.js
@@ -1,9 +1,116 @@
 import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
 export default class MakeNew extends Component {
+    constructor(props){
+        super(props);
+        
+        this.onChangeUsername = this.onChangeUsername.bind(this);   
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeTiming = this.onChangeTiming.bind(this); 
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state ={
+            username:'',
+            description:'',
+            timings:0,
+            date: new Date(),
+            users:[]
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            users:'test user',
+            username:'test user'
+        })
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        });
+    }
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        }); 
+    }
+    onChangeTiming(e){
+        this.setState({
+            timings: e.target.value
+        }); 
+    }
+    onChangeDate(date){
+        this.setState({
+            date: date
+        }); 
+    }
+  
+    onSubmit(e){
+        e.preventDefault();
+        const appointment = {
+            username: this.state.username,
+            description: this.state.description,
+            timings: this.state.timings,
+            date: this.state.date
+        }
+        console.log(appointment);
+        window.location='/';
+    }
+
     render() {
         return(
             <div>
-                <p>You are on the Make New Component!</p>
+                <h3>Create New Appointment</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select ref="userInput"
+                                required
+                                className="form-control"
+                                value={this.state.username}
+                                onChange={this.onChangeUsername}>
+                                    {
+                                      this.state.users.map(function(user){
+                                          return <option
+                                          key={user}
+                                          value={user}>{user}
+                                          </option>;
+                                      })
+                                    }
+                        </select>
+                        </div>
+                    <div className="form-group">
+                            <label>Description: </label>
+                            <input type="text"
+                                    required
+                                    className="form-control"
+                                    value={this.state.description}
+                                    onChange={this.onChangeDescription} />
+                        </div>
+                    <div className="form-group">
+                            <label>Timing: </label>
+                            <input type="text"
+                                    required
+                                    className="form-control"
+                                    value={this.state.timings}
+                                    onChange={this.onChangeTiming} />
+                        </div>
+                    <div className="form-group">
+                            <label>Date: </label>
+                            <div>
+                                <DatePicker
+                                   selected={this.state.date}
+                                   onChange={this.onChangeDate}/>
+                            </div>
+                        </div>
+                <div className="form-group">
+                            <input type="submit" value="Create Appointment list" className="btn btn-primary"/>
+                        </div>
+                </form>
             </div>
         )
     }
